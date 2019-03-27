28b48f5659f8eb3f9ee8d51cdf5277c21f820fcb
ramdhon <ramadhonimuhammad@gmail.com>
2019-03-27 17:19:17 +0700

    adding_partials_routes_views

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const app = require('express')();
 const PORT = 3000;
-
+const routes = require('./routes');
+const bodyParser = require('body-parser');
 
 app.set('view engine', 'ejs');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true}));
 
+app.use('/', routes);
 
 app.listen(PORT, () => {
   console.log(`app running on port:${PORT}`);
